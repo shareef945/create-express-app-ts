ec2b8308f164fdd6f089cbb4e25dee7c65d739e9
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-02-08 15:28:18 +0000

    publishing to npm....

---
+++ create.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+
+// Get the project directory from the command line arguments
+const targetDir = process.argv[2] || process.cwd();
+
+// Get the directory of the package files
+const templateDir = path.join(__dirname, "template");
+
+// List of files to ignore
+const ignoreList = [".gitignore", ".npmignore", "node_modules", "create.js"];
+
+// Function to copy files recursively
+function copyFiles(srcDir, destDir, ignoreList) {
+  fs.readdirSync(srcDir).forEach((file) => {
+    if (ignoreList.includes(file)) {
+      return;
+    }
+
+    const srcFile = path.join(srcDir, file);
+    const destFile = path.join(destDir, file);
+
+    const stat = fs.statSync(srcFile);
+    if (stat.isDirectory()) {
+      fs.mkdirSync(destFile, { recursive: true });
+      copyFiles(srcFile, destFile, ignoreList);
+    } else {
+      fs.copyFileSync(srcFile, destFile);
+    }
+  });
+}
+
+// Start the copying process
+copyFiles(templateDir, targetDir, ignoreList);
