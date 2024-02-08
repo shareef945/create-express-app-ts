0aa4554adc37d9e5f75dc777945948debed99a9a
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-02-08 15:04:25 +0000

    publishing to npm....

--- install.js
+++ install.js
@@ -1,23 +1,29 @@
-#!/usr/bin/env node
 const fs = require("fs");
 const path = require("path");
 
 const targetDir = process.cwd();
-const templateDir = path.join(__dirname);
 
-function copyFiles(srcDir, destDir) {
+const templateDir = __dirname;
+
+const ignoreList = [".gitignore", ".npmignore", "node_modules", ".git"];
+
+function copyFiles(srcDir, destDir, ignoreList) {
   fs.readdirSync(srcDir).forEach((file) => {
+    if (ignoreList.includes(file)) {
+      return;
+    }
+
     const srcFile = path.join(srcDir, file);
     const destFile = path.join(destDir, file);
 
     const stat = fs.statSync(srcFile);
     if (stat.isDirectory()) {
       fs.mkdirSync(destFile, { recursive: true });
-      copyFiles(srcFile, destFile);
-    } else if (!file.startsWith(".") && !file.endsWith(".gitignore")) {
+      copyFiles(srcFile, destFile, ignoreList);
+    } else {
       fs.copyFileSync(srcFile, destFile);
     }
   });
 }
 
-copyFiles(templateDir, targetDir);
+copyFiles(templateDir, targetDir, ignoreList);
