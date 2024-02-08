f041c104b7bbf778a1ea0a051d91d2cd0288733c
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-02-08 15:50:33 +0000

    publishing to npm....

--- create.js
+++ create.js
@@ -4,18 +4,12 @@ const fs = require("fs");
 const path = require("path");
 const { execSync } = require("child_process");
 
-
-// Get the project directory from the command line arguments
 const targetDir = process.argv[2] || process.cwd();
 
-// Get the directory of the package files
 const templateDir = __dirname;
 
-// List of files to ignore
 const ignoreList = [".gitignore", ".npmignore", "node_modules", "create.js"];
 
-
-// Function to copy your template files from the package to the user's current directory
 function copyFiles(srcDir, destDir, ignoreList) {
   fs.readdirSync(srcDir).forEach((file) => {
     if (ignoreList.includes(file)) {
@@ -35,18 +29,17 @@ function copyFiles(srcDir, destDir, ignoreList) {
   });
 }
 
-// Function to install dependencies, if necessary
 function installDependencies() {
   execSync("npm install", { stdio: "inherit" });
 }
 
-// Main function to orchestrate the setup
 function main() {
   console.log("Setting up a new Node.js app...");
   copyFiles(templateDir, targetDir, ignoreList);
 
   installDependencies();
   console.log("Your Node.js app is ready!");
+  console.log("Check our documentation here - https://www.npmjs.com/package/create-node-app-scaffold");
 }
 
 main();
