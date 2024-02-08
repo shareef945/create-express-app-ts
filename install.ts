eb20ccaccc48bd26f69cca56a51f5a836696b2f9
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-02-08 14:19:14 +0000

    publishing to npm

---
+++ install.ts
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const targetDir = process.cwd();
+
+const templateDir = path.join(__dirname);
+
+function copyFiles(srcDir: string, destDir: string) {
+  fs.readdirSync(srcDir).forEach((file: any) => {
+    const srcFile = path.join(srcDir, file);
+    const destFile = path.join(destDir, file);
+
+    const stat = fs.statSync(srcFile);
+    if (stat.isDirectory()) {
+      fs.mkdirSync(destFile, { recursive: true });
+      copyFiles(srcFile, destFile);
+    } else if (!file.startsWith(".") && !file.endsWith(".gitignore")) {
+      fs.copyFileSync(srcFile, destFile);
+    }
+  });
+}
+
+copyFiles(templateDir, targetDir);
